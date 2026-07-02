3875e2dc4edf18144396e3f15186a53ad0f70f09
ignacio <ignacio.espinoza@virginiogomez.cl>
2026-07-02 14:32:22 -0400

    Allow Vercel hostname in ALLOWED_HOSTS

--- fuchivola/settings.py
+++ fuchivola/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-@!75ou%4vjn5)8i%h@1bw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,fuchivola.vercel.app', cast=Csv())
 
 
 # Application definition
